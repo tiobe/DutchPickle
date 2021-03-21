9e072c110ddbf0c6f3e7f853e72d466f1e97c08f
jansen <jansen@a1ace184-460c-0410-9a26-c72b8d09e5c3>
2021-03-21 19:27:08 +0000

    27809: [Gherkin] Introduced new rule 'Repeating setup steps (in the beginning) of every Scenario should be part of the Background'.

---
+++ checker/src/main/java/com/tiobe/gherkin/Rule14.java
@@ -0,0 +1,52 @@
+package com.tiobe.gherkin;
+
+import com.tiobe.antlr.GherkinParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rule14 extends Rule {
+    public Rule14(final List<Violation> violations) {
+        super(violations);
+    }
+
+    public String getSynopsis() {
+        return "Repeating setup steps of every Scenario should be part of the Background";
+    }
+
+    public void check(final GherkinParser.MainContext ctx) {
+        int numberOfCommonPrefixes = 0;
+        int numberOfScenarios = 0;
+        List<GherkinParser.StepContext> previousScenario = new ArrayList<>();
+        for (GherkinParser.InstructionContext instruction : ctx.instruction()) {
+            if (instruction.stepInstruction() != null && instruction.stepInstruction().scenario() != null) {
+                if (previousScenario.isEmpty()) {
+                    numberOfCommonPrefixes = instruction.step().size();
+                } else {
+                    final int prefixes = getNumberOfPrefixes(previousScenario, instruction.step());
+                    numberOfCommonPrefixes = Math.min(prefixes, numberOfCommonPrefixes);
+                    if (numberOfCommonPrefixes == 0) {
+                        break; // no need to continue, there is no prefix
+                    }
+                }
+                previousScenario = instruction.step();
+                numberOfScenarios++;
+            }
+        }
+
+        if (numberOfScenarios > 1 && numberOfCommonPrefixes > 0) {
+            addViolation(14, ctx, "All Scenarios in this Feature share " + numberOfCommonPrefixes + " setup Steps that could be put in a Background");
+        }
+    }
+
+    int getNumberOfPrefixes(final List<GherkinParser.StepContext> scenario1, final List<GherkinParser.StepContext> scenario2) {
+        int prefixes = 0;
+        for (; prefixes < Math.min(scenario1.size(), scenario2.size()); prefixes++) {
+            if (!scenario1.get(prefixes).getText().equals(scenario2.get(prefixes).getText())) {
+                break;
+            }
+        }
+
+        return prefixes;
+    }
+}
---
+++ checker/src/test/resources/com/tiobe/gherkin/Rule14.feature
@@ -0,0 +1,21 @@
+Feature: bla # Violation
+
+Scenario: text
+Given bla
+When foo
+Then true
+
+Scenario: text2
+Given bla
+When foo
+Then true2
+
+Scenario: xxyyy
+#some text
+Given bla
+When foo
+Examples:
+| x | y |
+
+
+
